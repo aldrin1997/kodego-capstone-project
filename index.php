7e0db88c192eaa22a414e7ff172f9716b17f49bf
CathMonsales <cathmonsales03@gmail.com>
2023-07-04 05:41:27 +0800

    adjusted button size

--- index.php
+++ index.php
@@ -27,30 +27,30 @@ if (isset($_SESSION['username'])) {
     <div class="container">
       <div class="navbar-brand">
         <img src="image/logo.png" width="300px" class="logo-image" alt="Logo">
-        <span class="logo-text d-flex font-size-responsive">MAC Lending Inc.</span>
       </div>
+      <span class="logo-text d-flex font-size-responsive">MAC Lending Inc.</span>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ms-auto">
           <li class="nav-item">
-            <a class="nav-link text-success" href="index.php">Home</a>
+            <a class="nav-link fw-bold text-success" href="index.php">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="about.php">About</a>
+            <a class="nav-link fw-medium" href="about.php">About</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="services.php">Services</a>
+            <a class="nav-link fw-medium" href="services.php">Services</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="contact.php">Contact</a>
+            <a class="nav-link fw-medium" href="contact.php">Contact</a>
           </li>
           
         </ul>
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link btn btn-success" href="login.php">Login</a>
+            <a class="nav-link fw-medium btn btn-success" href="login.php">Login</a>
           </li>
         </ul>
       </div>
@@ -66,7 +66,7 @@ if (isset($_SESSION['username'])) {
           <h1>Loan for daily</h1>
           <h1>Expenses.</h1>
           <p>At MAC Lending, our vision is to provide fast approved loans for all types of individuals who are in need of quick financial assistance. We understand that sometimes life throws unexpected challenges our way, and having access to immediate funds can make a significant difference.</p>
-          <button class="btn btn-success btn-lg mt-4">Apply for Loan</button>
+          <button class="btn btn-success btn-sm mt-4">Apply for Loan</button>
         </div>
         <div class="col-lg-6">
           <img src="image/image1.png" alt="lending money" class="img-fluid">
@@ -86,7 +86,7 @@ if (isset($_SESSION['username'])) {
         <h3>Future & Good Support.</h3>
         <p>Our mission is to make the borrowing process as seamless and efficient as possible. We believe that everyone deserves equal opportunities when it comes to accessing financial support. Therefore, regardless of your credit history or financial background, we strive to offer fast loan approvals to all eligible applicants.</p>
         
-        <button class="btn btn-success btn-lg mt-4">Apply for Loan</button>
+        <button class="btn btn-success btn-sm mt-4">Apply for Loan</button>
       </div>
       <div class="col-lg-6">
         <img src="image/image2.png" alt="lending money" class="img-fluid">
@@ -105,7 +105,7 @@ if (isset($_SESSION['username'])) {
         <h3>Building a Brighter financial</h3>
         <h3>Future & Good Support.</h3>
         <p>With our streamlined application process, you can apply for a loan with ease. Simply visit our website or reach out to our dedicated customer service team, and we'll guide you through the necessary steps. We have simplified the documentation requirements to expedite the approval process, allowing you to receive the funds you need quickly.</p>
-        <button class="btn btn-success btn-lg mt-4">Apply for Loan</button>
+        <button class="btn btn-success btn-sm mt-4">Apply for Loan</button>
       </div>
       <div class="col-lg-6">
         <img src="image/image3.png" alt="lending money" class="img-fluid">
@@ -120,17 +120,17 @@ if (isset($_SESSION['username'])) {
   <div class="card">
     <img src="image/image4.png" alt="Personal Loan" class="img-fluid">
     <h5>Personal Loan</h5>
-    <button class="btn btn-success">Apply For Loan</button>
+    <button class="btn btn-success btn-sm">Apply Now!</button>
   </div>
   <div class="card">
     <img src="image/image5.png" alt="Salary Loan" class="img-fluid">
     <h5>Salary Loan</h5>
-    <button class="btn btn-success">Apply For Loan</button>
+    <button class="btn btn-success btn-sm">Apply Now!</button>
   </div>
   <div class="card">
     <img src="image/image6.png" alt="Small Business Loan" class="img-fluid">
     <h5>Small Business Loan</h5>
-    <button class="btn btn-success">Apply For Loan</button>
+    <button class="btn btn-success btn-sm">Apply Now!</button>
   </div>
 </div>
 <br>
